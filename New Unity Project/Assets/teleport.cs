c24bb3d52981321c567f4be87707aac8ec06c825
maoxianxian <c7ye@UCSD.EDU>
2018-02-02 19:47:06 -0800

    friday night

--- New Unity Project/Assets/teleport.cs
+++ New Unity Project/Assets/teleport.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class teleport : MonoBehaviour {
 	GameObject ground;
 	RaycastHit hit;
+	float time=0;
 
 	// Use this for initialization
 	void Start () {
 		ground = GameObject.FindGameObjectWithTag ("floor");
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		//if (Physics.Raycast (transform.position, transform.forward, out hit)) {
-			
-		//}
+		Debug.Log (transform.transform.rotation);
+		if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			//Debug.Log (hit.rigidbody.gameObject.transform.parent.gameObject.transform.parent.gameObject);
+			if (hit.rigidbody.gameObject == ground) {
+				time += Time.deltaTime;
+				Debug.Log (time);
+				if (time > 3) {
+					time = 0;
+					transform.position = hit.point;
+				}
+			} else {
+				time = 0;
+			}
+		}
 	}
 }
