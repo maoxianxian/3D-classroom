d4189f71882b20753c96c9e30651a94039b6d034
maoxianxian <c7ye@UCSD.EDU>
2018-02-02 19:12:50 -0800

    integrate touch

---
+++ New Unity Project/Assets/teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour {
+	GameObject ground;
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		ground = GameObject.FindGameObjectWithTag ("floor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			
+		//}
+	}
+}
