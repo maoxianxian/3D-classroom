b887c193d52568456c74843fe8741daca84221e7
maoxianxian <c7ye@ucsd.edu>
2018-02-03 17:02:21 -0800

    begin second select

--- New Unity Project/Assets/teleport.cs
+++ New Unity Project/Assets/teleport.cs
@@ -6,17 +6,23 @@ public class teleport : MonoBehaviour {
 	GameObject ground;
 	RaycastHit hit;
 	GameObject lefthand;
+	GameObject left_far_hand;
 	GameObject righthand;
 	GameObject current=null;
+	GameObject closePlayer;
+	GameObject farPlayer;
 	Color lineColor=new Color(1,1,1);
 	float holdtime=0;
-
+	bool raycastmode=true;
 	// Use this for initialization
 	void Start () {
 		ground = GameObject.FindGameObjectWithTag ("floor");
-		lefthand = GameObject.Find ("hand_left");
-		righthand = GameObject.Find ("hand_right");
-
+		closePlayer = GameObject.FindGameObjectWithTag ("closeplayer");
+		lefthand = closePlayer.transform.GetChild(2).gameObject;
+		righthand = closePlayer.transform.GetChild(4).gameObject;
+		farPlayer = GameObject.Find ("LocalAvatar 2");
+		left_far_hand = farPlayer.transform.GetChild (2).gameObject;
+		farPlayer.SetActive (false);
 	}
 	// Update is called once per frame
 	void Update () {
@@ -38,44 +44,62 @@ public class teleport : MonoBehaviour {
 			//Debug.Log (OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles);
 			//Debug.Log(GameObject.Find ("hand_left").transform.forward);
 			//Debug.Log(GameObject.Find ("controller_left").transform.forward);
+		}
 
-		}
-		//teleport
-		if(OVRInput.Get(OVRInput.Button.One,OVRInput.Controller.LTouch)){
-			if (Physics.Raycast (lefthand.transform.position, lefthand.transform.forward, out hit)) {
-				if (hit.transform.gameObject == ground) {
-					GameObject player = GameObject.FindGameObjectWithTag ("Player");
-					player.GetComponent<CharacterController> ().Move(hit.point-	player.transform.position);
-				}
-			}
-		}
-		//raycast select
-		holdtime+=Time.deltaTime;
-		if(OVRInput.Get(OVRInput.Button.Two,OVRInput.Controller.LTouch)){
-			if (Physics.Raycast (lefthand.transform.position, lefthand.transform.forward, out hit)) {
-				if (current == null && holdtime>1) {
-					GameObject currenthit = hit.transform.gameObject;
-					if (currenthit != ground && currenthit.tag != "wall") {
-						current = currenthit;
-						current.transform.parent = null;
-						current.transform.SetParent (lefthand.transform);
-						holdtime = 0;
-						current.GetComponent<Rigidbody>().isKinematic = true;
-					} 
-				} else {
-					if (holdtime < 1) {
-						holdtime += Time.deltaTime;
-					} else {
-						current.transform.SetParent (GameObject.Find("wall").transform);
-						current.GetComponent<Rigidbody>().isKinematic = false;
-						current = null;
-						holdtime = 0;
+		//raycast
+		if (raycastmode) {
+			//teleport
+			if(OVRInput.Get(OVRInput.Button.One,OVRInput.Controller.LTouch)){
+				if (Physics.Raycast (lefthand.transform.position, lefthand.transform.forward, out hit)) {
+					if (hit.transform.gameObject == ground) {
+						GameObject player = GameObject.FindGameObjectWithTag ("Player");
+						player.GetComponent<CharacterController> ().Move(hit.point-	player.transform.position);
 					}
 				}
 			}
+			//select
+			holdtime += Time.deltaTime;
+			if (OVRInput.Get (OVRInput.Button.Two, OVRInput.Controller.LTouch)) {
+				if (Physics.Raycast (lefthand.transform.position, lefthand.transform.forward, out hit)) {
+					if (current == null && holdtime > 1) {
+						GameObject currenthit = hit.transform.gameObject;
+						if (currenthit != ground && currenthit.tag != "wall") {
+							current = currenthit;
+							current.transform.parent = null;
+							current.transform.SetParent (lefthand.transform);
+							holdtime = 0;
+							current.GetComponent<Rigidbody> ().isKinematic = true;
+						} 
+					} else {
+						if (holdtime < 1) {
+							holdtime += Time.deltaTime;
+						} else {
+							current.transform.SetParent (GameObject.Find ("wall").transform);
+							current.GetComponent<Rigidbody> ().isKinematic = false;
+							current = null;
+							holdtime = 0;
+						}
+					}
+				}
+			}
+			if (current != null) {
+				current.transform.position += 0.1f * lefthand.transform.forward * OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick) [1];
+			}
+		} else {//gogo
+			
 		}
-		if (current != null) {
-			current.transform.position += 0.1f*lefthand.transform.forward * OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick) [1];
+	}
+
+	void switchmode()
+	{
+		if (raycastmode) {
+			farPlayer.SetActive (true);
+			closePlayer.SetActive (false);
+			raycastmode = false;
+		} else {
+			farPlayer.SetActive (false);
+			closePlayer.SetActive (true);
+			raycastmode = true;
 		}
 	}
 
